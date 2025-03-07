bacf6fddd0ebc30fd96359cbe4bae7a4698e8b5c
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-08 00:22:50 +0700

    Update SO N-Day qty projection.py

--- SO N-Day qty projection.py
+++ SO N-Day qty projection.py
@@ -76,7 +76,7 @@ if so_file and dry_forecast_file and fresh_cbn_forecast_file and fresh_pgs_forec
                                                     daily_result['Sum of multiplier']) * daily_result['Sum of multiplier']
         daily_result[f'Predicted SO Qty D+{day}'] = daily_result[f'Predicted SO Qty D+{day}'].clip(lower=0).astype(int)
 
-        sample_wh = daily_result[(daily_result["wh_id"] == 40) & (daily_result["hub_id"] == 121)].head()
+        sample_wh = daily_result[(daily_result["wh_id"] == 160) & (daily_result["hub_id"] == 121)].head()
         st.dataframe(sample_wh[["Sum of maxqty", "Updated Hub Qty D+1", "Sum of multiplier", "Predicted SO Qty D+1"]])
 
 
