d35123904c542a2ba32a896224880ecedbf6712a
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-07 23:54:09 +0700

    Update SO N-Day qty projection.py

--- SO N-Day qty projection.py
+++ SO N-Day qty projection.py
@@ -47,9 +47,9 @@ if so_file and dry_forecast_file and fresh_cbn_forecast_file and fresh_pgs_forec
         daily_fresh_cbn_forecast = fresh_cbn_forecast_df[fresh_cbn_forecast_df["date_key"] == forecast_date]["Forecast Step 3"].sum()
         daily_fresh_pgs_forecast = fresh_pgs_forecast_df[fresh_pgs_forecast_df["date_key"] == forecast_date]["Forecast Step 3"].sum()
 
-        st.write(dry_forecast_df.head())
-        st.write(fresh_cbn_forecast_df.head())
-        st.write(fresh_pgs_forecast_df.head())
+        st.write(daily_dry_forecast.head())
+        st.write(daily_fresh_cbn_forecast.head())
+        st.write(daily_fresh_pgs_forecast.head())
 
         
         # Allocate Demand Forecast to WHs
