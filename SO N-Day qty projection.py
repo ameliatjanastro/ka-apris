2b3dd4a67c362847162383f70a074b2a499e3dfb
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-07 18:48:13 +0700

    Update SO N-Day qty projection.py

--- SO N-Day qty projection.py
+++ SO N-Day qty projection.py
@@ -70,11 +70,10 @@ if so_file and dry_forecast_file and fresh_cbn_forecast_file and fresh_pgs_forec
                 final_so_df.loc[hub_mask, 'forecast_based_so'] = 0  # If no SO, assign 0
     
             # Ensure deviation is calculated for each WH × Hub
-            final_so_df['Deviation (%)'] = ((final_so_df.loc[hub_mask2, 'Sum of qty_so_final'] - final_so_df.loc[hub_mask2, 'forecast_based_so']) /
-                                                final_so_df.loc[hub_mask2, 'forecast_based_so'].replace(0, pd.NA)) * 100
+            final_so_df['Deviation Qty'] = ((final_so_df.loc[hub_mask2, 'Sum of qty_so_final'] - final_so_df.loc[hub_mask2, 'forecast_based_so'])
                 
             # Fill NaN values with 0 for cases where forecast_based_so was originally 0
-            final_so_df['Deviation (%)'] = final_so_df['Deviation (%)'].fillna(0)
+            final_so_df['Deviation Qty'] = final_so_df['Deviation Qty'].fillna(0)
 
 
     # Display Results
