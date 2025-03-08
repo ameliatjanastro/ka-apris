cc630f06d5b4aefe26bd57ff92e379b5579e296a
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-08 17:05:30 +0700

    Update SO N-Day qty projection.py

--- SO N-Day qty projection.py
+++ SO N-Day qty projection.py
@@ -189,6 +189,10 @@ if so_file:
         st.dataframe(styled_filtered_so, column_config={col: st.column_config.TextColumn(width="small") for col in filtered_so_df.columns}, use_container_width=True)
         #st.dataframe(filtered_so_df[["Hub Name", "Sum of qty_so", "Predicted SO Qty D+0", "Sum of qty_so_final"]],column_config={col: st.column_config.TextColumn(width="small") for col in filtered_so_df.columns})
 
+        csv1 = filtered_so_df.to_csv(index=False).encode('utf-8')
+        st.download_button("Download Next Day SO Prediction", csv, "next_day_so_prediction.csv", "text/csv")
+   
+    
     with tab2:
             
         # Initialize result DataFrame
