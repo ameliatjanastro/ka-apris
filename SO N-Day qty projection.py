43b1b81a7005726187008b04ea90694d4774c61a
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-03-10 16:36:19 +0700

    Update SO N-Day qty projection.py

--- SO N-Day qty projection.py
+++ SO N-Day qty projection.py
@@ -224,14 +224,6 @@ if so_file:
                     else:
                         hub_forecast = 0
 
-                    previous_day_qty = 0
-                    if day > 1:
-                        previous_day_qty = daily_result.loc[hub_mask, f'Predicted SO Qty D-{day-1}'].fillna(0).astype(int)
-                    
-                    daily_result.loc[hub_mask, f'Updated Hub Qty D+{day}'] = (
-                        daily_result.loc[hub_mask, f'Updated Hub Qty D+{day}'] - hub_forecast + previous_day_qty
-                    ).clip(lower=0)
-
                     #daily_result.loc[hub_mask, f'Updated Hub Qty D+{day}'] -= hub_forecast
                     daily_result.loc[hub_mask, f'Updated Hub Qty D+{day}'] = daily_result.loc[hub_mask, f'Updated Hub Qty D+{day}'].clip(lower=0)
             
