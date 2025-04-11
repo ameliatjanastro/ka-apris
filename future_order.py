0798ff401f584b5a2404b88f556e4a74b7e4994b
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-04-11 22:26:44 +0700

    Update future_order.py

--- future_order.py
+++ future_order.py
@@ -79,7 +79,7 @@ def calculate_columns(df, cycle):
                 df['assumed_ospo_qty'] = df['rl_qty_new']
                 df['ospo for future'] = df['rl_qty_new']
                 df['rl_qty_future'] = 
-                    df['max_stock_wh']
+                    (df['max_stock_wh']
                     - df['assumed_stock_wh']
                     - df['assumed_ospo_qty']
                     #+ df['rl_qty_hub']
