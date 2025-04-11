168a00dd13a5010bb4bdeadfcbff4b6db1df117f
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-04-11 11:53:41 +0700

    Update future_order.py

--- future_order.py
+++ future_order.py
@@ -135,15 +135,13 @@ def main():
 
         # Calculate columns based on selected cycle
         df = calculate_columns(df, cycle)
-        columns_to_display = [
+        # Display the modified dataframe with future order dates, assumed stock, and assumed OSPO
+        st.write(df[
         'product_id', 'product_name', 'avg_sales_final', 
         'vendor_id', 'vendor_name', 'location_id', 'doi_policy', 
         'max_stock_wh', 'assumed_stock_wh', 'ospo for future',  # Ensure this column exists
         'rl_qty_future', 'landed_doi'
-        ]
-        df = df[columns_to_display]
-        # Display the modified dataframe with future order dates, assumed stock, and assumed OSPO
-        st.write(df)
+        ])
 # Run the app
 if __name__ == "__main__":
     main()
