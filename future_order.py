7c513637f55486d17aab07c0b7aca463a65c0b47
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-04-12 02:15:33 +0700

    Update future_order.py

--- future_order.py
+++ future_order.py
@@ -55,37 +55,37 @@ def calculate_columns(df, cycle):
             - df['assumed_ospo_qty']
         ).fillna(0).clip(lower=0).round()
         df['landed_doi'] = (df['assumed_stock_wh'] / (df['avg_sales_final'] * df['JI'])).fillna(0).clip(lower=0).round()
-        df[f'assumed_ospo_qty_1'] = df['assumed_ospo_qty']
-        df[f'assumed_stock_wh_1'] = df['assumed_stock_wh']
-        df['rl_qty_amel_1'] = df['rl_qty_amel']
+    df[f'assumed_ospo_qty_1'] = df['assumed_ospo_qty']
+    df[f'assumed_stock_wh_1'] = df['assumed_stock_wh']
+    df['rl_qty_amel_1'] = df['rl_qty_amel']
 
-        df['avg_sales_future_cycle'] = df['avg_sales_final'] * (1 + np.random.uniform(-0.2, 0.1, size=len(df)))
-        df.sort_values(by=['product_id', 'location_id', 'next_order_date'], inplace=True)
-        for i in range(2, cycle_num + 1):
-            df[f'assumed_ospo_qty_{i}'] = (
-                df.groupby(['product_id', 'location_id'])[f'rl_qty_amel_{i-1}']
+    df['avg_sales_future_cycle'] = df['avg_sales_final'] * (1 + np.random.uniform(-0.2, 0.1, size=len(df)))
+    df.sort_values(by=['product_id', 'location_id', 'next_order_date'], inplace=True)
+    for i in range(2, cycle_num + 1):
+        df[f'assumed_ospo_qty_{i}'] = (
+            df.groupby(['product_id', 'location_id'])[f'rl_qty_amel_{i-1}']
                 .shift(1)
                 .fillna(0)
             )
     
-            df[f'assumed_stock_wh_{i}'] = (
-                df.groupby(['product_id', 'location_id'])[f'assumed_stock_wh_{i-1}']
+        df[f'assumed_stock_wh_{i}'] = (
+            df.groupby(['product_id', 'location_id'])[f'assumed_stock_wh_{i-1}']
                 .shift(1)
                 .fillna(0)
                 + df[f'assumed_ospo_qty_{i}']
             )
     
-            df[f'rl_qty_amel_{i}'] = (
-                df['max_stock_wh']
-                - df[f'assumed_stock_wh_{i}']
-                - df[f'assumed_ospo_qty_{i}']
+        df[f'rl_qty_amel_{i}'] = (
+            df['max_stock_wh']
+            - df[f'assumed_stock_wh_{i}']
+            - df[f'assumed_ospo_qty_{i}']
             ).fillna(0).clip(lower=0).round()
 
-            df['assumed_stock_wh'] = df[f'assumed_stock_wh_{i}']
-            df['assumed_ospo_qty'] = df[f'assumed_ospo_qty_{i}']
-            df['rl_qty_amel'] = df[f'rl_qty_amel_{i}']
+        df['assumed_stock_wh'] = df[f'assumed_stock_wh_{i}']
+        df['assumed_ospo_qty'] = df[f'assumed_ospo_qty_{i}']
+        df['rl_qty_amel'] = df[f'rl_qty_amel_{i}']
 
-            df['landed_doi'] = (df['assumed_stock_wh'] / (df['avg_sales_final'] * df['JI'])).fillna(0).clip(lower=0).round()
+        df['landed_doi'] = (df['assumed_stock_wh'] / (df['avg_sales_final'] * df['JI'])).fillna(0).clip(lower=0).round()
 
     return df
 
