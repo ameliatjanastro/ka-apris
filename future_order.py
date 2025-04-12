e48cec0fc260ac527104de6752c9a180fa7c66a3
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-04-12 23:47:09 +0700

    Update future_order.py

--- future_order.py
+++ future_order.py
@@ -174,7 +174,7 @@ def main():
         cycle_options = ['Current'] + [f'Cycle {i}' for i in range(1, num_cycles + 1)]
         selected_cycle = st.selectbox("Select Cycle", cycle_options)
 
-        result_df, summary_df = calculate_columns(df.copy(), selected_cycle)
+        result_df = calculate_columns(df.copy(), selected_cycle)
 
         # Show only selected columns
         cols_to_show = [
