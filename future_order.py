290638393111533cd98a4e0fd945f2a533cccb73
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-04-17 15:23:18 +0700

    Update future_order.py

--- future_order.py
+++ future_order.py
@@ -210,10 +210,10 @@ def calculate_columns(df, cycle, frequency_df,forecast_df):
         merged = df.merge(frequency_df, on=merge_columns, how='left')
         merged['selisih_hari'] = merged['selisih_hari'].fillna('0')
         #selisih_days = str(row['selisih_hari']).split(',')
-        base_date = pd.to_datetime(row['future_inbound_date'], format='%d-%b-%Y', errors='coerce')
+        base_date = pd.to_datetime(merged['future_inbound_date'], format='%d-%b-%Y', errors='coerce')
         future_date_freq = base_date + merged['selisih_hari']
-        vendor_freq = float(row['vendor_frequency']) if row['vendor_frequency'] else 1
-        qty_per_day_freq = row['rl_qty_amel'] / vendor_freq
+        vendor_freq = float(merged['vendor_frequency']) if merged['vendor_frequency'] else 1
+        qty_per_day_freq = merged['rl_qty_amel'] / vendor_freq
 
         summary_distribution2 = (
                     df.groupby(['primary_vendor_name','vendor_frequency'])
