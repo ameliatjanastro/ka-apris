5c4a4c9b02c3fdc041d9f9adcfea26b235a64874
ameliatjanastro <amelia.tjandra@astronauts.id>
2025-05-21 23:14:10 +0700

    Update instantpoia.py

--- instantpoia.py
+++ instantpoia.py
@@ -35,8 +35,9 @@ merged_df['inbound_day_num'] = merged_df['inbound_day'].map(weekday_mapping)
 # 4. Create Result DataFrame
 # Identify key columns for the output
 print("Available columns:", merged_df.columns.tolist())
-print("Requested columns:", identifying_cols)
 identifying_cols = ['product_id', 'location_id', 'primary_vendor_name']
+print("Requested columns:", identifying_cols)
+
 unique_product_locations = merged_df[identifying_cols].drop_duplicates()
 
 # DEBUG: Print columns of unique_product_locations
